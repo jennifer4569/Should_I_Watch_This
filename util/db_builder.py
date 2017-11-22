c6d9110b52e28fc847534b82f26f23e2f2490e9a
ibnullify <ijahan1@stuy.edu>
2017-11-22 12:59:56 -0500

    database methods

--- util/db_builder.py
+++ util/db_builder.py
@@ -17,7 +17,19 @@ def create_db():
     command = "CREATE TABLE movie(overview TEXT, reviews REAL, recommendations TEXT, ratings TEXT)"
     c.execute(command)
 
+def add_user(username, password):
+    command = "SELECT id FROM users ORDER BY id DESC"
+    c.execute(command)
+    id = c.fetchone()
+    if (id == None):
+        id = 0
+    id = id[0] +1
+    command = "INSERT INTO users VALUES(" + str(id) + ",'" + username + "','" + password + "','','')"
+    c.execute(command)
+
+    
 create_db();
+add_user("hi","hey")
 
 db.commit() #save changes
 db.close()  #close database
